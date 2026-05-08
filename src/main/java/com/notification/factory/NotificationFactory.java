ac07512dce8e58ae8bba3f9f9e54143e250e626e
mai-triinkaseoja <mai.triin.kaseoja@gmail.com>
2026-05-08 16:47:01 +0300

    Add debug level logging

--- src/main/java/com/notification/factory/NotificationFactory.java
+++ src/main/java/com/notification/factory/NotificationFactory.java
@@ -5,6 +5,8 @@ import com.notification.adapter.SendGridAdapter;
 import com.notification.adapter.TwilioAdapter;
 import com.notification.core.EmailProvider;
 import com.notification.core.SmsProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.regions.Region;
 
 import java.io.IOException;
@@ -12,6 +14,8 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class NotificationFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationFactory.class);
 
     private final Properties properties;
 
@@ -27,6 +31,7 @@ public class NotificationFactory {
                 throw new RuntimeException("notification.properties faili ei leitud");
             }
             props.load(input);
+            logger.debug("Konfiguratsioon laaditud edukalt");
         } catch (IOException e) {
             throw new RuntimeException("Konfiguratsiooni laadimine ebaõnnestus", e);
         }
@@ -35,7 +40,9 @@ public class NotificationFactory {
 
     public SmsProvider createSmsSender() {
         String provider = properties.getProperty("notification.provider.sms");
+        logger.debug("SMS teenusepakkuja valimine: {}", provider);
         if ("twilio".equalsIgnoreCase(provider)) {
+            logger.debug("TwilioAdapter loomine");
             return new TwilioAdapter(
                 properties.getProperty("twilio.account.sid"),
                 properties.getProperty("twilio.auth.token"),
@@ -47,13 +54,16 @@ public class NotificationFactory {
 
     public EmailProvider createEmailSender() {
         String provider = properties.getProperty("notification.provider.email");
+        logger.debug("E-posti teenusepakkuja valimine: {}", provider);
         if ("sendgrid".equalsIgnoreCase(provider)) {
+            logger.debug("SendGridAdapter loomine");
             return new SendGridAdapter(
                 properties.getProperty("sendgrid.api.key"),
                 properties.getProperty("sendgrid.from.email")
             );
         }
         if ("ses".equalsIgnoreCase(provider)) {
+            logger.debug("AmazonSesAdapter loomine");
             return new AmazonSesAdapter(
                 properties.getProperty("ses.from.email"),
                 Region.of(properties.getProperty("ses.region"))
